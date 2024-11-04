f543df8c0c31cd646660d94bae796b463f943d3e
Aslam <aslam>
2024-11-05 02:05:32 +0530

    update

--- lib/screens/create_note.dart
+++ lib/screens/create_note.dart
@@ -1,4 +1,4 @@
-  import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 class CreateNote extends StatefulWidget {
   const CreateNote({super.key});
@@ -8,12 +8,40 @@ class CreateNote extends StatefulWidget {
 }
 
 class _CreateNoteState extends State<CreateNote> {
+  final titleController = TextEditingController();
+  final noteController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Create Note'),
-
-    ));
+        title: const Text('Add Note'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: titleController,
+              style: const TextStyle(fontSize: 24),
+              decoration: const InputDecoration(
+                hintText: 'Title',
+                border: InputBorder.none,
+              ),
+              autofocus: true,
+              maxLines: 1,
+            ),
+            TextFormField(
+              controller: noteController,
+              decoration: const InputDecoration(
+                hintText: 'Note',
+                border: InputBorder.none,
+              ),
+              maxLines: null,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,6 +14,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
+        centerTitle: true,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
